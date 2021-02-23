1a6901e9e454727d5e6579a9a5dc37354f173f17
Liguo Dong <donglg13@163.com>
2021-02-23 13:16:08 +0800

    Add files via upload

--- Fig6_files.R
+++ Fig6_files.R
@@ -49,13 +49,82 @@ t.test((rowMeans(GLU_HA_1_promoter_cf[72:204,])+rowMeans(GLU_HA_2_promoter_cf[72
 
 #Figure 6F######################
 
-#programming is same with Figure 6A
+#get single or multiple CreA binding TF genes
+full_list_transcription_factors_v2<-read_files_into_character("/Users/dongliguo/Documents/ANAlyses_folder/CreA_Summary/final_plots/Fig4/full_list_transcription_factors.xls")
+mutiple_genes<-read_files_into_character("/Users/dongliguo/Documents/ANAlyses_folder/CreA_Summary/final_plots/Fig2/gene_list_with_mutiple_binding.xls")
+signle_genes<-read_files_into_character("/Users/dongliguo/Documents/ANAlyses_folder/CreA_Summary/final_plots/Fig2/gene_list_with_single_binding.xls")
+write.table(mutiple_genes[mutiple_genes%in%full_list_transcription_factors_v2],file="mutiple_binding_TFs.xls",row.names=FALSE,col.names=FALSE,quote=FALSE,sep="\t")
+write.table(signle_genes[signle_genes%in%full_list_transcription_factors_v2],file="signle_binding_TFs.xls",row.names=FALSE,col.names=FALSE,quote=FALSE,sep="\t")
+
+
+#make tiles for the binding and calculate binding density
+tiles_2<-make_tiles_for_promoter_ATG("signle_binding_TFs.xls")
+GLU_WT_promoter_single<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_BIA1_CreA_1.bam.bed")
+GLU_WT_HA_promoter_single<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_MH11036_CreA_1.bam.bed")
+GLU_S4_1_promoter_single<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_S4_CreA_1.bam.bed")
+GLU_S4_2_promoter_single<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_S4_CreA_2.bam.bed")
+GLU_HA_1_promoter_single<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_HA_CreA_1.bam.bed")
+GLU_HA_2_promoter_single<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_HA_CreA_2.bam.bed")
+
+#make tiles for the binding and calculate binding density
+tiles_2<-make_tiles_for_promoter_ATG("mutiple_binding_TFs.xls")
+GLU_WT_promoter_multiple<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_BIA1_CreA_1.bam.bed")
+GLU_WT_HA_promoter_multiple<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_MH11036_CreA_1.bam.bed")
+GLU_S4_1_promoter_multiple<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_S4_CreA_1.bam.bed")
+GLU_S4_2_promoter_multiple<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_S4_CreA_2.bam.bed")
+GLU_HA_1_promoter_multiple<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_HA_CreA_1.bam.bed")
+GLU_HA_2_promoter_multiple<-normalization_tiles_for_promoter("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_HA_CreA_2.bam.bed")
+
+#plot the figure
+plot(colMeans(GLU_S4_1_promoter_single),type="l",ylim=c(150,1000),col="green",lwd=2)
+lines(colMeans(GLU_S4_1_promoter_multiple),type="l",ylim=c(150,1000),col="orange",lwd=2)
 
 ################################
 
 #Figure 6G######################
 
-#programming is same with Figure 6B
+#read gene expression and TF gene list
+full_list_transcription_factors_v2<-read_files_into_character("/Users/dongliguo/Documents/ANAlyses_folder/CreA_Summary/final_plots/Fig4/full_list_transcription_factors.xls")
+mutiple_genes<-read_files_into_character("/Users/dongliguo/Documents/ANAlyses_folder/CreA_Summary/final_plots/Fig2/gene_list_with_mutiple_binding.xls")
+signle_genes<-read_files_into_character("/Users/dongliguo/Documents/ANAlyses_folder/CreA_Summary/final_plots/Fig2/gene_list_with_single_binding.xls")
+
+#find summits for each binding around TF gene promoters
+summit_bed_files<-read.table("CreA_summits_and_annotation_per_annotation_v12.bed",sep="\t")
+summit_bed_files_v2<-summit_bed_files[summit_bed_files[,5]%in%"promoter",]
+write.table(summit_bed_files_v2[summit_bed_files_v2[,6]%in%full_list_transcription_factors_v2,c(1,2,3,6,2)],file="TF_binding_density_summits.bed",row.names=FALSE,col.names=FALSE,quote=FALSE,sep="\t")
+write.table(summit_bed_files_v2[!summit_bed_files_v2[,6]%in%full_list_transcription_factors_v2,c(1,2,3,6,2)],file="others_binding_density_summits.bed",row.names=FALSE,col.names=FALSE,quote=FALSE,sep="\t")
+
+#make tiles for the summits and get the corresponding signals
+source("/Users/dongliguo/Documents/ANAlyses_folder/AcuK_M_FacB_Summary/FacB_analysis/make_tiles_functions.R")
+normalization_tiles_for_summits_counts<-function(x)
+        {
+                M_0h<-import.bed(x)
+                start(ranges(M_0h))[as.character(strand(M_0h))=="-"]=start(ranges(M_0h))[as.character(strand(M_0h))=="-"]
+                width(ranges(M_0h))[as.character(strand(M_0h))=="+"]=1
+                M_0h.p=countOverlaps(tiles_2,M_0h)
+                M_0h.p.matrix=matrix(M_0h.p,nrow=length(tiles_2)/3001,ncol=3001,byrow=TRUE)
+                l<-length(M_0h)
+                M_0h.p.matrix2<-M_0h.p.matrix
+                for ( i in 1:length(M_0h.p.matrix[,1]) )
+                        {
+                                for ( j in 1:length(M_0h.p.matrix[i,]))
+                                {
+                                       M_0h.p.matrix2[i,j]<-M_0h.p.matrix[i,j]*1000000/(0.05*l)
+                                }
+                        }
+                return(M_0h.p.matrix2)
+
+        }
+TF_binding_density_out<-read.table("TF_binding_density_summits.bed")
+others_binding_density_out<-read.table("others_binding_density_summits.bed")
+tiles_2<-make_tiles_for_summit_percise("TF_binding_density_summits.bed")
+CreA_glucose_TFs<-normalization_tiles_for_summits_counts("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_S4_CreA_1.bam.bed")
+tiles_2<-make_tiles_for_summit_percise("others_binding_density_summits.bed")
+CreA_glucose_others<-normalization_tiles_for_summits_counts("/Volumes/LiguoDisk/CreA_RNA_files/CreA_new_ChIP_seqs_files/bam_files_out/GLU_S4_CreA_1.bam.bed")
+
+#plot the figures
+boxplot(rowMaxs(CreA_glucose_TFs[,1450:1550]),rowMaxs(CreA_glucose_others[,1450:1550]),outline=FALSE)
+boxplot(rowMeans(CreA_glucose_TFs[,1475:1525]),sample(rowMeans(CreA_glucose_others[,1475:1525]),length(rowMeans(CreA_glucose_TFs[,1475:1525]))),col=c("red","gray"),outline=TRUE,ylim=c(0,800),pch=16)
 
 ################################
 
---
+++ FigS7_files.R
@@ -0,0 +1,19 @@
+
+#Figure S7####################
+
+#The data and functions following other Figure script
+full_list_transcription_factors_v2<-integrate_list[integrate_list%in%full_list_transcription_factors]
+TF_expression_values<-plot_heatmaps_RNA_seq_v2(full_list_transcription_factors_v2)[,1]
+TF_binding_level<-plot_heatmaps_bindings(full_list_transcription_factors_v2)[,2]
+my_palette <- colorRampPalette(c("skyblue2", "skyblue2","whitesmoke","whitesmoke","tomato", "tomato"))(n = 100)
+ratio_matrix_plot<-t(matrix(TF_expression_values[order(TF_expression_values,decreasing=TRUE)],nrow=4))
+input_values<-t(matrix(convert_gene_list(full_list_transcription_factors_v2)[order(TF_expression_values,decreasing=TRUE)],nrow=4))
+input_values<-as.expression(lapply(full_list_transcription_factors_v2[order(TF_expression_values)],function(a) bquote(italic(.(a)))))
+TF_binding_level_plot<-t(matrix(TF_binding_level[order(TF_expression_values,decreasing=TRUE)],nrow=4))
+TF_binding_level_plot[TF_binding_level_plot==1]="_        "
+TF_binding_level_plot[TF_binding_level_plot==0]=""
+
+#plot the figures
+heatmap.2(ratio_matrix_plot,symm=T,scale='none',symkey=T,symbreaks=T,sepcolor="gray",trace=c("none"),cexRow = 1.3,density.info=c("none"),col=my_palette[1:100],Colv=FALSE,Rowv=FALSE,breaks=seq(-4,4,length.out=101),colsep=0:ncol(as.matrix(ratio_matrix_plot)),rowsep=0:nrow(as.matrix(ratio_matrix_plot)),sepwidth=c(0.001,0.00001),cellnote=TF_binding_level_plot,notecol="black",notecex=1.2)
+
+##############################
